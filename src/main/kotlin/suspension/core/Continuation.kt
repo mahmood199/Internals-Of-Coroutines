f2ac81812c87bc9ff5eefbedf06d106e099af483
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-15 00:16:15 +0530

    documentation
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- src/main/kotlin/suspension/core/Continuation.kt
+++ src/main/kotlin/suspension/core/Continuation.kt
@@ -8,7 +8,21 @@ import kotlin.coroutines.CoroutineContext
  */
 
 interface Continuation<in T> {
+    /**
+     * Context of the coroutine that corresponds to this continuation.
+     */
     val context: CoroutineContext
+
+    /**
+     * Resumes the execution of the corresponding coroutine by passing
+     * [value] as the return value of the last suspension point.
+     */
     fun resume(value: T)
+
+    /**
+     * Resumes the execution of the corresponding coroutine
+     * so that the [exception] is re-thrown right after the
+     * last suspension point.
+     */
     fun resumeWithException(exception: Throwable)
 }
