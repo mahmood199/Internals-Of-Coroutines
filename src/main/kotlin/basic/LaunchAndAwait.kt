fb12fab09fdd00956aa5f438629a4b7e87dbf082
MSI KATANA <mohammed.mahmood@semaai.com>
2023-04-12 04:36:41 +0530

    Added launch and await

---
+++ src/main/kotlin/basic/LaunchAndAwait.kt
@@ -0,0 +1,13 @@
+package basic
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+
+suspend fun main() {
+    val x = GlobalScope.async {
+        delay(3000L)
+        "1223"
+    }
+    println(x.await())
+}
